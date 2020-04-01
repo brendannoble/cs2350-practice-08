07bd87336351d14ef16d91e4536282ddaec2b241
Brendan Noble <brendann755@gmail.com>
2020-04-01 10:44:43 -0600

    Version 1

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,7 +1,47 @@
-// Required by Webpack - do not touch
 require.context('../', true, /\.(html|json|txt|dat)$/i)
 require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
-// JavaScript
-//TODO
+import { movies } from './movies';
+
+let featured_movie = document.querySelector('.featured');
+for(let m of movies) {
+  let movie_thumbnail = document.getElementById('m' + m.id);
+  movie_thumbnail.innerHTML = `
+    <img src="${m.poster}" />
+  `;
+
+  movie_thumbnail.onclick = () => {
+    selectMovie(m);
+  }
+}
+
+const selectMovie = (m) => {
+  featured_movie.innerHTML = `
+    <img src="${m.poster}" style="float: left;" />
+    <h1>${m.title}</h1>
+    <p>${m.plot}</p>
+  `
+}
+
+const searchMovies = (event) => {
+  if (event) {
+    event.preventDefault();
+  }
+
+  let input = document.querySelector('[type="search"').value || "";
+  for (let m of movies) {
+    let movie_thumbnail = document.getElementById('m' + m.id);
+    if (m.title.toUpperCase().indexOf(input.toUpperCase()) == -1) {
+      // movie_thumbnail.classList.add('hidden');
+      movie_thumbnail.style.display = 'none';
+    } else {
+      // movie_thumbnail.classList.remove('hidden');
+      movie_thumbnail.style.display = 'block';
+    }
+  }
+}
+
+document.querySelector('button').onclick = searchMovies;
+document.querySelector('[type="search"').onsearch = searchMovies;
+document.forms[0].addEventListener('submit', searchMovies, false);
